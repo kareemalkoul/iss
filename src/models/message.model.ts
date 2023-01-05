5e355cd71cc3117bd13c3fe1a38d3a4b228619e7
kareemalkoul <kareemalkoul1986@gmail.com>
2023-01-05 12:09:26 +0200

    encraption message in task 4

--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -8,6 +8,9 @@ import {
     BelongsTo,
 } from "sequelize-typescript";
 
+import CryptoJS from "crypto-js"
+
+
 import { Chat } from "./chat.model";
 @Table({ tableName: "message" })
 export class Message extends Model {
@@ -21,8 +24,20 @@ export class Message extends Model {
     @Column
     chat_id!: number;
 
-    @Column({ allowNull: false })
+    @Column({
+        allowNull: false,
+        get(this) {
+            const text = this.getDataValue("text") ?? ""
+            var decrypted = CryptoJS.AES.decrypt(text, "WERTYKDF34BSJ");
+            return decrypted.toString(CryptoJS.enc.Utf8);
+        },
+        set(text: string) {
+            var encrypted = CryptoJS.AES.encrypt(text, "WERTYKDF34BSJ");
+            this.setDataValue("text", encrypted.toString())
+        }
+    })
     text!: string
+
 
     @Column({ defaultValue: false })
     is_read!: boolean
